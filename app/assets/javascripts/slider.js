fa2307b3df062f8d1223682bea1f04e10e4ddbbd
rudolfvavra <rudolfvavra@gmail.com>
2016-03-03 18:07:00 +0100

    Add
      - examination.css button styling
      - add foundation icon fonts
      - add .bower

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -23,22 +23,21 @@ $(window).resize(function() {
         // dots: true,
       });
     }
+    if((windowWidth > 1) && (windowWidth < 640)){
+      $('.slick-multislider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        // dots: true,
+      });
+    } else {
+      $('.slick-multislider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        // dots: true,
+      });
+    }
 });
-
-if((windowWidth > 1) && (windowWidth < 640)){
-  $('.slick-multislider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    // dots: true,
-  });
-} else {
-  $('.slick-multislider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    // dots: true,
-  });
-}
